aebcd00a1cca653a7f63e34db8071efd7ed91691
githigaurav <gfxshaolin@gmail.com>
2023-09-06 16:42:14 +0530

    mongDB Cluster Added

--- server/connection/connection.js
+++ server/connection/connection.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 const URL="mongodb://127.0.0.1:27017/userAuthentication"
+// const DBURL=`mongodb+srv://${process.env.DBUSERNAME}:${process.env.DBPASSWORD}@server.ln65acq.mongodb.net/?retryWrites=true&w=majority`
 mongoose.connect(URL)
 .then(()=>{
     console.log("db Connection established")
